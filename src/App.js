f2bd8eb1d7ead28e567a9ffea88f598f01f5eba9
raviranjan2003 <2140131@sliet.ac.in>
2024-03-09 23:13:47 +0530

    Text changed in HomePage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Ravii !
+          Welcome to Devops !
         </p>
         <a
           className="App-link"
